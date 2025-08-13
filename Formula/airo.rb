12e21b44bb9c24abbfdc16a42a9efed2e3464256
Roman Chudov <roman.chudov@gmail.com>
2025-08-13 06:52:26 +0300

    v1.12.1

--- Formula/airo.rb
+++ Formula/airo.rb
@@ -1,8 +1,8 @@
 class Airo < Formula
   desc "Spec-first Golang project scaffold generator"
   homepage "https://github.com/rom8726/airo"
-  url "https://github.com/rom8726/airo/releases/download/v1.12.0/airo_darwin_arm64"
-  sha256 "b2f79dc4448fe26d10191f520b6c93f5ad1058622f4cac4cc27da47daf9b56a8"
+  url "https://github.com/rom8726/airo/releases/download/v1.12.1/airo_darwin_arm64"
+  sha256 "4e41069410a89ab5ad4e55dfb47a8b0e45f790f0c518ddc16ac909478757920b"
   license "Apache-2.0"
 
   def install
